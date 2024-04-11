608484734ff0293555c2161a1701fe01aa1c99a2
Michael <152283283+JustDarkWish@users.noreply.github.com>
2024-04-11 16:55:44 +0200

    Aggiunto limite caratteri nella text area

--- Esercizio Gruppo - Twitter/JS/scriptPostPage.js
+++ Esercizio Gruppo - Twitter/JS/scriptPostPage.js
@@ -2,5 +2,18 @@ let userNicknameSpan = document.querySelector('#userNicknameSpan');
 let nicknameAndIcon = document.querySelector('#nicknameAndIcon')
 let textArea1 = document.querySelector('#textArea1');
 let logoutFunction = document.querySelector('#logoutFunction');
+let remainingChar = document.querySelector('#remainingChar');
 
 userNicknameSpan.textContent = JSON.parse(localStorage.getItem('login')).username;
+
+let count = 50;
+
+textArea1.addEventListener('input', e => {
+    remainingChar.textContent = count - textArea1.value.length;
+
+    if (textArea1.value.length > 49) {
+        textArea1.value = textArea1.value.substr(0,49);
+        remainingChar.textContent = 0;
+    }
+})
+
