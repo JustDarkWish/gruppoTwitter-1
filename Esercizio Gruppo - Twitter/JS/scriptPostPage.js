2cf238ac6aaf06b95f2d69fbb3d3b1fa08398ab9
Michael <152283283+JustDarkWish@users.noreply.github.com>
2024-04-11 17:44:32 +0200

    problema post

--- Esercizio Gruppo - Twitter/JS/scriptPostPage.js
+++ Esercizio Gruppo - Twitter/JS/scriptPostPage.js
@@ -3,6 +3,8 @@ let nicknameAndIcon = document.querySelector('#nicknameAndIcon')
 let textArea1 = document.querySelector('#textArea1');
 let logoutFunction = document.querySelector('#logoutFunction');
 let remainingChar = document.querySelector('#remainingChar');
+let textAreaDiv = document.querySelector('#textAreaDiv');
+let postsContainer = document.querySelector('#postsContainer')
 
 userNicknameSpan.textContent = JSON.parse(localStorage.getItem('login')).username;
 
@@ -15,5 +17,13 @@ textArea1.addEventListener('input', e => {
         textArea1.value = textArea1.value.substr(0,49);
         remainingChar.textContent = 0;
     }
+})
+
+textAreaDiv.addEventListener('submit', e => {
+    e.preventDefault();
+    let post = textArea1.value;
+
+    postsContainer.innerHTML += `<div class="coloreBgTweet mt-5 py-4 border d-flex flex-wrap"><p class="p-4">${post}</p>
+</div>`
 })
 
