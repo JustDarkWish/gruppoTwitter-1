1d731c721cc7ce4c2054169b86076a827e0c129a
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2024-04-11 18:04:39 +0200

    text fix

--- Esercizio Gruppo - Twitter/JS/scriptPostPage.js
+++ Esercizio Gruppo - Twitter/JS/scriptPostPage.js
@@ -1,29 +1,30 @@
-let userNicknameSpan = document.querySelector('#userNicknameSpan');
-let nicknameAndIcon = document.querySelector('#nicknameAndIcon')
-let textArea1 = document.querySelector('#textArea1');
-let logoutFunction = document.querySelector('#logoutFunction');
-let remainingChar = document.querySelector('#remainingChar');
-let textAreaDiv = document.querySelector('#textAreaDiv');
-let postsContainer = document.querySelector('#postsContainer')
+let userNicknameSpan = document.querySelector("#userNicknameSpan");
+let nicknameAndIcon = document.querySelector("#nicknameAndIcon");
+let textArea1 = document.querySelector("#textArea1");
+let logoutFunction = document.querySelector("#logoutFunction");
+let remainingChar = document.querySelector("#remainingChar");
+let textAreaDiv = document.querySelector("#textAreaDiv");
+let postsContainer = document.querySelector("#postsContainer");
 
-userNicknameSpan.textContent = JSON.parse(localStorage.getItem('login')).username;
+userNicknameSpan.textContent = JSON.parse(
+  localStorage.getItem("login")
+).username;
 
 let count = 50;
 
-textArea1.addEventListener('input', e => {
-    remainingChar.textContent = count - textArea1.value.length;
+textArea1.addEventListener("input", (e) => {
+  remainingChar.textContent = count - textArea1.value.length;
 
-    if (textArea1.value.length > 49) {
-        textArea1.value = textArea1.value.substr(0,49);
-        remainingChar.textContent = 0;
-    }
-})
+  if (textArea1.value.length > 49) {
+    textArea1.value = textArea1.value.substr(0, 49);
+    remainingChar.textContent = 0;
+  }
+});
 
-textAreaDiv.addEventListener('submit', e => {
-    e.preventDefault();
-    let post = textArea1.value;
+textAreaDiv.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let post = textArea1.value;
 
-    postsContainer.innerHTML += `<div class="coloreBgTweet mt-5 py-4 border d-flex flex-wrap"><p class="p-4">${post}</p>
-</div>`
-})
-
+  postsContainer.innerHTML += `<div class="coloreBgTweet mt-5 py-4 px-2 border text-break "><p>${post}</p>
+</div>`;
+});
